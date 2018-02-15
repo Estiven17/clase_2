021963f1e702bb0e496455fc1c36313584228b44
Estiven17 <36452076+Estiven17@users.noreply.github.com>
2018-02-15 13:07:42 -0500

    Add files via upload

--- clase_2/main.cpp
+++ clase_2/main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>
+#include <iostream> //bibliteca i/o
 
-using namespace std;
+using namespace std;//palabras reservadas
 
-int main()
+int main() // funcion principal
 {
-   int num1,num2,num3;
+   float num1,num2,num3; //declaracion de variables
+  // pedir datos a el usuario
    cout <<"ingrese el primer numero: ";
    cin >>num1;
    cout <<"ingrese el segundo numero: ";
    cin >>num2;
    cout<<"ingrese el tercer numero: ";
    cin >> num3;
+
+   //proceso
    if  (num1>num2 && num1>num3)
    {
         cout <<""<<endl;
